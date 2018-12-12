9348bf272a11fbca9d3c1514d83a5cf2be41f77d
Max Milner <maxmilner31@yahoo.com>
2018-12-12 16:07:33 -0600

    done listener done

--- src/scripts/Components/Predix/eventListeners.js
+++ src/scripts/Components/Predix/eventListeners.js
@@ -1,47 +1,47 @@
-import submitPredix from "./submitPredix"
-import deletePredix from "./deletePredix"
+import submitPredix from "./submitPredix";
+import deletePredix from "./deletePredix";
 import doneEditPredix from "./doneEdit";
 
 const eventListening = {
   submitButton() {
-    document.addEventListener("click", (e) => {
-      if (e.target.id === "subPredix")
-      console.log("submit to me")
-      submitPredix()
-    })
+    document.addEventListener("click", e => {
+      if (e.target.id === "subPredix") console.log("submit to me");
+      submitPredix();
+    });
   },
 
   deleteButton(e) {
-      const predixId = e.target.parentNode.id
-      console.log("parentid", predixId)
-        deletePredix(predixId)
-    },
+    const predixId = e.target.parentNode.id;
+    console.log("parentid", predixId);
+    deletePredix(predixId);
+  },
 
   editButton() {
-    console.log("clicked editing?")
-    document.addEventListener("click", (e) => {
+    console.log("clicked editing?");
+    document.addEventListener("click", e => {
       if (e.target.id === "editPick") {
-        let id = e.target.className
-        let userId = sessionStorage.user_id
-        let predixPicture = document.querySelector("#predixPicture")
+        let id = e.target.className;
+        let userId = sessionStorage.user_id;
+        let predixPicture = document.querySelector("#predixPicture");
         let obj = {
           picture: predixPicture.value,
           user_id: userId
-        }
-        console.log("editing?", obj)
+        };
+        console.log("editing?", obj);
       }
-    })
+    });
   },
 
   doneButton() {
-    document.addEventListener("click", (e) => {
-      if (e.target.id === "donePick")
-        console.log("done?")
-        console.log(e.target.id)
-        doneEditPredix()
-         predixDom()
-    })
+    document.addEventListener("click", e => {
+      if (e.target.id === "donePick") {
+        console.log("done?");
+        console.log(e.target.id);
+        doneEditPredix();
+        predixDom();
+      }
+    });
   }
-}
+};
 
-export default eventListening
+export default eventListening;
